3e42c617022212e329d34b59e9612bd96624279c
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2019-11-29 19:47:14 +0100

    edit friendship model

--- web/web/domain/models/friendships.py
+++ web/web/domain/models/friendships.py
@@ -1,26 +1,25 @@
 # web/domain/friendships.py
-# from sqlalchemy import Column, Integer, ForeignKey
-# from sqlalchemy.orm import relationship
-# from sqlalchemy.utils.types.choice import ChoiceType
-#
-# from web.domain import db
-# from web.domain.models.behaviors import IdMixin
-#
-#
-# class Friendship(IdMixin, db.Model):
-#     STATUS = [
-#         (1, "Pending"),
-#         (2, "Accepted"),
-#         (3, "Declined"),
-#         (4, "Blocked"),
-#     ]
-#
-#     __tablename__ = "friendships"
-#
-#     user_one_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-#     user_two_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-#     action_user = Column(Integer, ForeignKey("users.id"), nullable=False)
-#     status = Column(ChoiceType(STATUS, impl=Integer()), nullable=False)
-#     user_one = relationship("User", foreign_keys=[user_one_id])
-#     user_two = relationship("User", foreign_keys=[user_two_id])
-#     action_user = relationship("User", foreign_keys=[action_user])
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+from web.domain import db
+from web.domain.models.behaviors import IdMixin
+
+STATUS = {
+    1: "Pending",
+    2: "Accepted",
+    3: "Declined",
+    4: "Blocked",
+}
+
+
+class Friendship(IdMixin, db.Model):
+    __tablename__ = "friendships"
+
+    user_one_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_two_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    action_user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    status = Column(Integer, nullable=False)
+    user_one = relationship("User", foreign_keys=[user_one_id])
+    user_two = relationship("User", foreign_keys=[user_two_id])
+    action_user = relationship("User", foreign_keys=[action_user_id])
