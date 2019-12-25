46979391e0a5eaef75704d5c723d4ffb6f5d7a1a
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2019-12-25 22:30:10 +0100

    add tests

--- web/web/tests/component/test_friendship_api.py
+++ web/web/tests/component/test_friendship_api.py
@@ -1,5 +1,7 @@
 import unittest
 
+import pytest
+
 from web.tests.component.mixins import BaseTestCase, FriendshipMixin, UserMixin
 from web.tests.factories import UserFactory
 
@@ -162,6 +164,14 @@ class TestFriendshipBlueprint(UserMixin, FriendshipMixin, BaseTestCase):
         expected_json = {"results": []}
         self.assertEqual(expected_json, self.response.json)
 
+    @pytest.mark.skip(reason="TODO")
+    def test_check_invitations_which_user_sent(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_check_invitations_which_user_sent_when_no_invitations_have_been_sent(self):
+        pass
+
     def test_user_accept_invitation_from_another_user(self):
         self.__given_two_users_and_one_sent_invitation()
         self.json = self.__prepare_dict_with_username(self.users_data[0]["username"])
@@ -287,133 +297,48 @@ class TestFriendshipBlueprint(UserMixin, FriendshipMixin, BaseTestCase):
             headers=action_user_headers, json=json
         )
 
-    #
-    # @pytest.mark.skip(reason="TO DO")
-    # def test_when_user_delete_friend_from_friendships(self):
-    #     self.__given_three_registered_users()
-    #     self.__when_first_user_send_invitation_to_second_user()
-    #     self.__when_second_user_accept_invitation_from_first_user()
-    #     self.__when_second_user_delete_first_user_from_friendship()
-    #     self.__then_user_get_204_when_friendship_has_been_deleted()
-    #
-    # @pytest.mark.skip(reason="TO DO")
-    # def test_when_user_delete_friend_and_have_only_one_friend(self):
-    #     self.__given_three_registered_users()
-    #     self.__when_first_user_send_invitation_to_second_user()
-    #     self.__when_second_user_accept_invitation_from_first_user()
-    #     self.__when_second_user_send_invitation_to_third_user()
-    #     self.__when_third_user_accept_invitation_from_second_user()
-    #     self.__when_second_user_delete_first_user_from_friendship()
-    #     self.__when_second_user_check_if_have_any_friends()
-    #     self.__then_second_user_have_third_user_in_friendships()
-    #
-    # def __given_three_registered_users(self):
-    #     self.user_data = [UserFactory.build() for i in range(3)]
-    #     self.headers = []
-    #
-    #     for user_data in self.user_data:
-    #         self.register_user(user_data)
-    #         login_response = self.login_user(user_data)
-    #         user_header = self.prepare_user_header(login_response.json["token"])
-    #         self.headers.append(user_header)
-    #
-    # def __when_first_user_send_invitation_to_second_user(self):
-    #     json = {"username": self.user_data[1]["username"]}
-    #     self.response = self.send_invitation_to_user(headers=self.headers[0], json=json)
-    #
-    # def __when_first_user_send_invitation_to_unknown_user(self):
-    #     json = {"username": "Test"}
-    #     self.response = self.send_invitation_to_user(headers=self.headers[0], json=json)
-    #
-    # def __when_second_user_send_invitation_to_third_user(self):
-    #     json = {"username": self.user_data[2]["username"]}
-    #     self.response = self.send_invitation_to_user(headers=self.headers[1], json=json)
-    #
-    # def __when_second_user_check_invitations_from_another_users(self):
-    #     self.response = self.get_invitations_from_users(headers=self.headers[1])
-    #
-    # def __when_second_user_accept_invitation_from_first_user(self):
-    #     json = {"username": self.user_data[0]["username"]}
-    #     self.response = self.send_accept_invitation(headers=self.headers[1], json=json)
-    #
-    # def __when_third_user_accept_invitation_from_second_user(self):
-    #     json = {"username": self.user_data[1]["username"]}
-    #     self.response = self.send_accept_invitation(headers=self.headers[2], json=json)
-    #
-    # def __when_second_user_check_if_have_any_friends(self):
-    #     self.response = self.get_list_of_friends(headers=self.headers[1])
-    #
-    # def __when_first_user_check_if_have_any_friends(self):
-    #     self.response = self.get_list_of_friends(headers=self.headers[0])
-    #
-    # def __when_second_user_decline_invitation_from_first_user(self):
-    #     json = {"username": self.user_data[0]["username"]}
-    #     self.response = self.send_decline_invitation(headers=self.headers[1], json=json)
-    #
-    # def __when_second_user_delete_first_user_from_friendship(self):
-    #     json = {"username": self.user_data[0]["username"]}
-    #     self.response = self.send_delete_freindship(headers=self.headers[1], json=json)
-    #
-    # def __then_user_get_201_when_invitation_has_been_successfully_send(self):
-    #     self.assertEqual(201, self.response.status_code)
-    #     expected_json = {"status": "success"}
-    #     self.assertEqual(expected_json, self.response.json)
-    #
-    # def __then_user_get_400_when_invitations_has_been_send_twice(self):
-    #     self.assertEqual(400, self.response.status_code)
-    #     expected_json = {"status": "fail", "message": "Already exists."}
-    #     self.assertEqual(expected_json, self.response.json)
-    #
-    # def __then_return_400_when_user_send_invitation_to_unknown_user(self):
-    #     self.assertEqual(400, self.response.status_code)
-    #     expected_json = {"status": "fail", "message": "User does not exist."}
-    #     self.assertEqual(expected_json, self.response.json)
-    #
-    # def __then_invition_from_first_user_is_waiting(self):
-    #     self.assertEqual(200, self.response.status_code)
-    #     self.assertEqual(1, len(self.response.json["waiting_users"]))
-    #     self.assertEqual(
-    #         self.user_data[0]["username"], self.response.json["waiting_users"][0]
-    #     )
-    #
-    # def __then_second_user_get_204_when_invitation_has_been_successfully_accepted(self):
-    #     self.assertEqual(204, self.response.status_code)
-    #     expected_json = {"status": "success"}
-    #     self.assertEqual(expected_json, self.response.json)
-    #
-    # def __then_second_user_have_friend(self):
-    #     response_json = self.response.json
-    #     self.assertEqual(200, self.response.status_code)
-    #     self.assertEqual(1, len(response_json["user_friends"]))
-    #     self.assertEqual(
-    #         self.user_data[0]["username"], response_json["user_friends"][0]
-    #     )
-    #
-    # def __then_second_user_have_third_user_in_friendships(self):
-    #     response_json = self.response.json
-    #     self.assertEqual(200, self.response.status_code)
-    #     self.assertEqual(1, len(response_json["user_friends"]))
-    #     self.assertEqual(
-    #         self.user_data[2]["username"], response_json["user_friends"][0]
-    #     )
-    #
-    # def __then_first_user_have_friend(self):
-    #     response_json = self.response.json
-    #     self.assertEqual(200, self.response.status_code)
-    #     self.assertEqual(1, len(response_json["user_friends"]))
-    #     self.assertEqual(
-    #         self.user_data[1]["username"], response_json["user_friends"][0]
-    #     )
-    #
-    # def __then_user_get_204_when_invitation_has_been_successfully_declined(self):
-    #     self.assertEqual(204, self.response.status_code)
-    #     expected_json = {"status": "success"}
-    #     self.assertEqual(expected_json, self.response.json)
-    #
-    # def __then_user_get_204_when_friendship_has_been_deleted(self):
-    #     self.assertEqual(204, self.response.status_code)
-    #     expected_json = {"status": "success"}
-    #     self.assertEqual(expected_json, self.response.json)
+    def test_user_have_no_waiting_invitation_when_decline_invitation(self):
+        self.__given_user_who_decline_invitation()
+        self.__when_user_check_if_have_invitations_from_another_users(
+            action_user_header=self.users_data[1]["headers"]
+        )
+        self.__then_return_200_and_empty_pending_invitation_list()
+
+    def __given_user_who_decline_invitation(self):
+        self.users_data = self.create_users(number_of_users=2)
+        self.send_invitation(
+            action_user_data=self.users_data[0], to_user_data=self.users_data[1]
+        )
+        self.decline_invitation(
+            action_user_data=self.users_data[1], to_user_data=self.users_data[0]
+        )
+
+    def decline_invitation(self, action_user_data, to_user_data):
+        json = self.__prepare_dict_with_username(to_user_data["username"])
+        response = self.send_decline_invitation(
+            headers=action_user_data["headers"], json=json
+        )
+        self.assertEqual(200, response.status_code)
+
+    @pytest.mark.skip(reason="TODO")
+    def test_user_who_sent_invitation_can_decline_invitation(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_user_who_declined_own_invitation_have_no_pending_invitations(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_user_have_no_pending_invitation_when_action_user_decline_invitation(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_user_delete_friend_from_friends(self):
+        pass
+
+    @pytest.mark.skip(reason="TODO")
+    def test_deleted_user_not_in_list_of_friends(self):
+        pass
 
 
 if __name__ == "__main__":
