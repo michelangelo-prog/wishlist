106d69372e7bfb6e3b97372c0239769bf3049e4f
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-01-18 12:21:47 +0100

    move env variables to external file

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -2,10 +2,8 @@
 
 echo "Waiting for postgres..."
 
-while ! nc -z web-db 5432; do
+while ! nc -z $SQL_HOST $SQL_PORT; do
   sleep 0.1
 done
 
 echo "PostgreSQL started"
-
-python manage.py run -h 0.0.0.0
