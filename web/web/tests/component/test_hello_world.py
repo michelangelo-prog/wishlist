86143e576805b9c88d9d73bea4a7a30cde8b4c16
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-01-18 11:32:46 +0100

    add assert to hello world test

--- web/web/tests/component/test_hello_world.py
+++ web/web/tests/component/test_hello_world.py
@@ -8,7 +8,10 @@ class TestHelloWorldBlueprint(UserBaseTestCase):
     def test_hello_world(self):
         uri = "/helloworld"
         response = self.client.get(uri, headers=self.headers)
+
         self.assertEqual(200, response.status_code)
+        expected_json = {"message": "Hello World!"}
+        self.assertEqual(expected_json, response.json)
 
 
 if __name__ == "__main__":
